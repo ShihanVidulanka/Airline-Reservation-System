ab3dc487f055ade4c892ef4012202a30759301f5
Sathira443 <liyanapathiranasathira@gmail.com>
2022-03-28 22:52:55 +0530

    Flight Dispatcher Updated
    CSS files renamed
    Home updated

--- flight_dispatcher_flight_details.php
+++ flight_dispatcher_flight_details.php
@@ -8,7 +8,7 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/cancel_flight.css">
+  <link rel="stylesheet" href="css/flight_dispatcher_flight_details.css">
   <title>Flight Details</title>
 </head>
 
