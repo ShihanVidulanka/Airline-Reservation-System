a843f0d3ea9afa0c27694f1463fdfc551c00a727
Sathira443 <liyanapathiranasathira@gmail.com>
2022-03-29 01:36:22 +0530

    Main Page Created

---
+++ main_page.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>B AIRWAYS - MAIN PAGE</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/fontAwesome.css">
+    <link rel="stylesheet" href="css/hero-slider.css">
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/datepicker.css">
+    <link rel="stylesheet" href="css/templatemo-style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <!--
+	Venue Template
+	http://www.templatemo.com/tm-522-venue
+-->
+</head>
+
+<body>
+
+    <div class="wrap">
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <button id="primary-nav-button" type="button">Menu</button>
+                        <a href="index.html">
+                            <div class="logo">
+                                <img src="img/logo5.png" alt="Venue Logo">
+                            </div>
+                        </a>
+                        <nav id="primary-nav" class="dropdown cf">
+                            <ul class="dropdown menu">
+                                <!-- <li class='active'><a href="#">Popular</a></li> -->
+                                <li><a href="#">Bookings</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Place Booking</a></li>
+                                        <li><a href="#">View Booking Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="scrollTo" data-scrollTo="blog" href="#">Destinations</a></li>
+
+                                <li><a class="scrollTo" data-scrollTo="about" href="#">Contact Us</a></li>
+                                <li><a class="scrollTo" href="#">Log In</a></li>
+                            </ul>
+                        </nav><!-- / #primary-nav -->
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+
+
+
+    <section class="banner" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="banner-caption">
+                        <div class="line-dec"></div>
+                        <h2>We Serve Always</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="featured-places" id="blog">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <span>Featured Places</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_1.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Paris</h4>
+                            <span>France</span>
+                            <p>Also known as the Latin Quarter, the 5th arrondissement is home to the Sorbonne
+                                university
+                                and student-filled cafes. It's also known for its bookshops, including the famed
+                                Shakespeare & Company.
+                                Family-friendly attractions include the Jardin des Plantes botanical gardens and the
+                                National Museum of Natural History.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_2.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Abu Dhabi</h4>
+                            <span>UAE</span>
+                            <p>Abu Dhabi, the capital of the United Arab Emirates, sits off the mainland on an island in
+                                the Persian (Arabian) Gulf. Its focus on oil exports and commerce is reflected
+                                by the skyline’s modern towers and shopping megacenters such as Abu Dhabi and Marina
+                                malls. Beneath white-marble domes, the vast Sheikh Zayed Grand Mosque features an
+                                immense Persian carpet, crystal chandeliers
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="featured-item">
+                        <div class="thumb">
+                            <img src="img/featured_item_3.jpg" alt="">
+                            <div class="overlay-content">
+                                <ul>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                    <li><i class="fa fa-star"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="down-content">
+                            <h4>Singapore</h4>
+                            <span>Singapore</span>
+                            <p>Singapore has been described as a playground for the rich, and it's true that the small
+                                city-state does have a certain sheen of wealth. But Singapore offers more than just
+                                high-end shopping malls, luxury hotels, and fine dining. There is also a vibrant history
+                                and diverse ethnic quarters
+                                to discover, along with many family-friendly attractions and lovely public spaces that
+                                make exploring this slightly futuristic city worthwhile.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container" id='about'>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="about-veno">
+                        <div class="logo">
+                            <img src="img/logo5.png" alt="Venue Logo">
+                        </div>
+                        <p>B airways is a leading airline in the world currently ranked 5th in the 
+                            Global airlines rankings.
+                        </p>
+                        <ul class="social-icons">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-rss"></i></a>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="contact-info">
+                        <div class="footer-heading">
+                            <h4>Contact Information</h4>
+                        </div>
+                        <!-- <p>Praesent iaculis gravida elementum. Proin fermentum neque facilisis semper pharetra. Sed
+                            vestibulum vehicula tincidunt.</p> -->
+                        <ul>
+                            <li><span>Phone:</span><a href="#">0719955178</a></li>
+                            <li><span>Email:</span><a href="#">contactus@bairways.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="sub-footer">
+        <p>Copyright &copy; 2018 Company Name
+
+            - Design: <a rel="nofollow" href="http://www.templatemo.com">Template Mo</a></p>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/datepicker.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
