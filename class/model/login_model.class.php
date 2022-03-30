93c23d88827e10a4c92bf6927634085445d83d8c
ShihanVidulanka <81677102+ShihanVidulanka@users.noreply.github.com>
2022-03-30 14:03:00 +0530

    login editted

--- class/model/login_model.class.php
+++ class/model/login_model.class.php
@@ -5,7 +5,7 @@ class Login_Model extends Dbh{
     protected function getUser($username, $password){
         $query = "SELECT * FROM user WHERE username='{$username}';";
         $result_set = mysqli_query($this->connect(), $query);
-
+        echo "hello";
         if (!$result_set) {
             //echo $result_set;
             header("location: login.php?error=ConnectionFails1");
@@ -28,56 +28,62 @@ class Login_Model extends Dbh{
         }
         elseif ($checkPassword == true) {
             // When Password is verified
+
+            $query2 =  "SELECT phone_no FROM telephone_no WHERE user_id ={$user["ID"]}";
+            $result_set2 = mysqli_query($this->connect(), $query2);
+
+            if (!($result_set2) ) {
+                header("location: login.php?error=ConnectionFail2");
+                exit();
+            }
+            $array_Tp_no = array();
+            while ($user_tp = mysqli_fetch_assoc($result_set2)) {
+                array_push($array_Tp_no, $user_tp['phone_no']);
+            }
+
             switch ($userType) {
                 case '0':
                     $user_details = $user;
+                    $_SESSION["ID"] = $user["ID"];
                     $_SESSION["username"] = $user["username"];
                     $_SESSION["account_type"] = $user["account_type"];
-                    $_SESSION["account_no"] = $user["account_no"];
+                    $_SESSION["TP_no"] = $array_Tp_no;
                     // header("location: airline_administrator_home.php");
                     return $userType;
                     break;
 
                 case '1':
-                    $query2 = "SELECT * FROM operations_agent WHERE account_no ={$user["account_no"]}";
+                    $query3 = "SELECT * FROM operations_agent WHERE user_id ={$user["ID"]}";
                     break;
 
                 case '2':
-                    $query2 = "SELECT * FROM flight_dispatcher WHERE account_no={$user["account_no"]}";
+                    $query3 = "SELECT * FROM flight_dispatcher WHERE user_id={$user["ID"]}";
                     break;
 
                 case '3':            
-                    $query2 = "SELECT * FROM registered_passenger WHERE account_no={$user["account_no"]}";
+                    $query3 = "SELECT * FROM registered_passenger WHERE user_id={$user["ID"]}";
                     break;
                 default:
                     header("location: login.php?error=UserNotFound2");
                     break;
             }
-            $query3 =  "SELECT * FROM telephone_no WHERE user_id ='{$user["ID"]}'";
-            $result_set2 = mysqli_query($this->connect(), $query2);
             $result_set3 = mysqli_query($this->connect(), $query3);
-
-            if (!($result_set2 && $result_set3) ) {
-                header("location: login.php?error=ConnectionFail2");
+            if (!$result_set3) {
+                header("location: login.php?error=ConnectionFail3");
                 exit();
             }
 
-            if (!mysqli_num_rows($result_set2) == 1) {
+            if (!mysqli_num_rows($result_set3) == 1) {
                 header("location: login.php?error=UserNotFound3");
                 exit();
             }
 
-            $user_details = mysqli_fetch_assoc($result_set2);
-            $user_TP = mysqli_fetch_assoc($result_set3);
-            $array_Tp_no = array();
-            foreach ($user_TP as $TP_no) {
-                array_push($array_Tp_no, $TP_no['phone_number']);
-            }
+            $user_details = mysqli_fetch_assoc($result_set3);
 
             session_start();
+            $_SESSION["ID"] = $user["ID"];
             $_SESSION["username"] = $user["username"];
             $_SESSION["account_type"] = $user["account_type"];
-            $_SESSION["account_no"] = $user["account_no"];
             $_SESSION["name"] = $user_details["name"];
             $_SESSION["TP_no"] = $array_Tp_no;
 
@@ -96,9 +102,9 @@ class Login_Model extends Dbh{
                     $_SESSION["NIC"] = $user_details["NIC"];
                     $_SESSION["age"] = $user_details["age"];
                     $_SESSION["passport_number"] = $user_details["passport_number"];
-                    $_SESSION["state"] = $user_details["state"];
                     $_SESSION["category"] = $user_details["category"];
                     $_SESSION["state"] = $user_details["state"];
+                    $_SESSION["passenger_id"] = $user_details["passenger_id"];
                     break;
                 default:
                     header("location: login.php?error=UserNotFound4");
