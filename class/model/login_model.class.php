18f1f2a9a6e2fe6f0a9455e338603eae922fd094
Yasith-Heshan <yheshan1@gmail.com>
2022-03-31 13:50:06 +0530

    comment delete

--- class/model/login_model.class.php
+++ class/model/login_model.class.php
@@ -1,6 +1,5 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/additional.inc.php";
-// require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/class/model/dbh.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/autoloader.inc.php";
 
 class Login_Model extends Dbh{
