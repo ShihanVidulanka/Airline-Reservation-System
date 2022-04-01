f677eb4f4ba9ea67067b3dd30c431109345be571
Sathira443 <liyanapathiranasathira@gmail.com>
2022-04-01 07:42:50 +0530

    POST function for specific button

---
+++ js/buttons.js
@@ -0,0 +1,25 @@
+//
+/**
+* Dynamically creates form elements and adds to $_POST
+* path     : the path to send the post request to
+* params   : The variables to be passed
+* method   : the method to use on the form default set to 'post'
+*/
+function post(path, params, method = 'post') {
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+
+    for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+            const hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+
+            form.appendChild(hiddenField);
+        }
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
