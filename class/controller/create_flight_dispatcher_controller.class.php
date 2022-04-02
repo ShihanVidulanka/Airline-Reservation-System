9cc87792d87dda7a171f6135222b201e2352ccaa
SahanDC <sahancaldera3@gmail.com>
2022-04-02 18:55:07 +0530

    amo amo

--- class/controller/create_flight_dispatcher_controller.class.php
+++ class/controller/create_flight_dispatcher_controller.class.php
@@ -3,7 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/addi
 require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/autoloader.inc.php";
 
 //Used flight_dispatcher.class.php created by Sathira I think.
-class CreateFlightDispatcher_Controller extends CreateFlightDispatcher_Model{
+class AirlineAdministrator_Controller extends AirlineAdministrator_Model{
     public function createFlightDispatcherFromModel(Flight_Dispatcher $flight_Dispatcher){
         $details = array(
             'username'=>remove_unnessaries($flight_Dispatcher->getUsername()),
@@ -14,20 +14,14 @@ class CreateFlightDispatcher_Controller extends CreateFlightDispatcher_Model{
             'airport_code'=>remove_unnessaries($flight_Dispatcher->getAirportCode()),
             'telephone_numbers'=>$flight_Dispatcher->getTelephoneNo()
         );
-        $this->createFlightDispatcher($details);
+        $this->createAccount($details, $details['account_type']);
     }
 
-    public function checkUsernameFromModel($username) {
-        $this->check_username(remove_unnessaries($username, 1));
-    }
-}
-
-class CreateOperationsAgent_Ccontroller extends CreateOperartionsAgent_Model{
     public function createOperationsAgentFromModel(Operations_Agent $operations_Agent){
         $details = array(
             'username'=>remove_unnessaries($operations_Agent->getUsername()),
             'hashed_password'=>"abcd",  // Question
-            'account_type'=>2,
+            'account_type'=>1,
             'first_name'=>remove_unnessaries($operations_Agent->getFirstName()),
             'last_name'=>remove_unnessaries($operations_Agent->getLastName()),
             'state'=>remove_unnessaries($operations_Agent->getState()),
@@ -35,11 +29,32 @@ class CreateOperationsAgent_Ccontroller extends CreateOperartionsAgent_Model{
             'telephone_numbers'=>$operations_Agent->getTelephoneNo()
         );
 
-        $this->createOperationsAgent($details);
+        $this->createAccount($details, $details['account_type']);
     }
 
     public function checkUsernameFromModel($username) {
         $this->check_username(remove_unnessaries($username, 1));
     }
 }
+
+// class CreateOperationsAgent_Ccontroller extends CreateOperartionsAgent_Model{
+//     public function createOperationsAgentFromModel(Operations_Agent $operations_Agent){
+//         $details = array(
+//             'username'=>remove_unnessaries($operations_Agent->getUsername()),
+//             'hashed_password'=>"abcd",  // Question
+//             'account_type'=>2,
+//             'first_name'=>remove_unnessaries($operations_Agent->getFirstName()),
+//             'last_name'=>remove_unnessaries($operations_Agent->getLastName()),
+//             'state'=>remove_unnessaries($operations_Agent->getState()),
+//             'airport_code'=>remove_unnessaries($operations_Agent->getAirportCode()),
+//             'telephone_numbers'=>$operations_Agent->getTelephoneNo()
+//         );
+
+//         $this->createOperationsAgent($details);
+//     }
+
+//     public function checkUsernameFromModel($username) {
+//         $this->check_username(remove_unnessaries($username, 1));
+//     }
+// }
 ?>
--- include/crerate_operations_agent.inc.php
+++ include/crerate_operations_agent.inc.php
@@ -3,7 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/addi
 require_once $_SERVER['DOCUMENT_ROOT']."/Airline-Reservation-System/include/autoloader.inc.php";
 
 if(isset($_POST['username_'])){
-    $create_operations_agent_controller = new CreateOperationsAgent_Ccontroller;
+    $create_operations_agent_controller = new AirlineAdministrator_Controller;
     $create_opereations_agent_controller->checkUsernameFromModel($_POST['username_']);
 }
 
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
     $_POST['telephone_numbers']=rtrim($_POST['telephone_numbers'],',');
     $_POST['telephone_numbers']=explode(",",$_POST['telephone_numbers']);
 
-    $create_operations_agent_controller = new CreateOperationsAgent_Ccontroller();
+    $create_operations_agent_controller = new AirlineAdministrator_Controller();
     // $operations_agent = new Operations_Agent();
     // Then set everything with ($_POST['everyrthing])
 
