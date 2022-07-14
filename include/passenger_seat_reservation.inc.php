98f6f913d89e1b97beaa13f735d76f5d6e363bdb
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-07-15 00:09:24 +0530

    fix some bugs

--- include/passenger_seat_reservation.inc.php
+++ include/passenger_seat_reservation.inc.php
@@ -6,7 +6,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/Airline-Reservation-System/include/au
 if(isset($_POST['seatno-flightid'])){
     $post = explode('-',$_POST['seatno-flightid']);
     $seat_reservation_view = new Seat_Reservation_View();
-    $reserved_seats = $seat_reservation_view->getReservedSeats($post[1]);
+    $reserved_seats = $seat_reservation_view->getReservedSeatsFromModel($post[1]);
     if(in_array($post[0],$reserved_seats)){
         echo 'alreadybooked';
     }else{
@@ -15,15 +15,16 @@ if(isset($_POST['seatno-flightid'])){
 }
 
 if(isset($_POST['flight_id'])){
-    print_array($_POST);
+    $coefficient = $_SESSION['coefficient'];
     $details=array(
         'passenger_id'=>$_SESSION['passenger_id'],
         'flight_id'=>$_POST['flight_id'],
-        'ticket_price'=>$_POST['ticket_price'],
+        'ticket_price'=>$_POST['ticket_price']*$coefficient,
         'seat_no'=>$_POST['seat_no'],
         'seat_type'=>$_POST['seat_type'],
         'state'=>2
     );
+    unset($_SESSION['coefficient']);
     $seat_reservation_controller = new Seat_Reservation_Controller();
     $seat_reservation_controller->reserveSeatFromModel($details);
 }
